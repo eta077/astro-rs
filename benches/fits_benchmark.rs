bd1f6e5c88a3664c99222352b727af726e5488b0
John Nystrom <66807409+eta077@users.noreply.github.com>
2022-06-11 15:13:23 -0500

    Add Get header 20 bench (#17)

--- benches/fits_benchmark.rs
+++ benches/fits_benchmark.rs
@@ -17,5 +17,17 @@ fn bench_get_header(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_get_header);
+fn bench_get_header_20(c: &mut Criterion) {
+    c.bench_function("get 20th header from fits file", |b| {
+        b.iter(|| {
+            let fits_file = black_box(File::open("assets/benchmarks/many_hdu.fits").unwrap());
+            let fits_file_reader = BufReader::new(fits_file);
+
+            let mut hdu_list = HduList::new(fits_file_reader);
+            hdu_list.get_by_index(20).unwrap().header.clone()
+        })
+    });
+}
+
+criterion_group!(benches, bench_get_header, bench_get_header_20);
 criterion_main!(benches);
